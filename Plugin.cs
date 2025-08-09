cea589e09e015c61784f8b23f900d30d81ee81cc
Heorhii <olenchenko.h.m@nmu.one>
2025-08-09 15:23:28 +0300

    GUI for moon

--- Plugin.cs
+++ Plugin.cs
@@ -3,6 +3,9 @@ using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 using MoonPhaseUtils.patches;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace MoonPhaseUtils;
 
@@ -11,11 +14,23 @@ public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
     static Harmony harmony;
-
+    public static Plugin Instance { get; private set; }
+    private static readonly object _updateLock = new();
+    
+    public GameObject? screenLabel;
+    public GameObject? screenImage;
+    public TextMeshProUGUI? screenLabelText;
+    public RawImage? screenImageTexture;
     private void Awake()
     {
         // Plugin startup logic 
         Logger = base.Logger;
+        
+        Instance = this;
+        
+        // Prevent the plugin from being deleted
+        gameObject.transform.parent = null;
+        gameObject.hideFlags = HideFlags.HideAndDontSave;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
         harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
         
@@ -41,4 +56,99 @@ public class Plugin : BaseUnityPlugin
         harmony.Patch(originalTwo, new HarmonyMethod(patchTwo));
         
     }
+    public void SetupLabel()
+    {
+        lock (_updateLock)
+        {
+            if (screenLabel == null) {
+                
+                
+                
+                Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID}: Creating label...");
+
+                GameObject hud = GameObject.Find("Game Hud");
+                GameObject haul = GameObject.Find("Tax Haul");
+
+                if (hud == null || haul == null)
+                {
+                    Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID}: Error getting HUD or Haul - not setting up label :(");
+                    return;
+                }
+				
+                TMP_FontAsset font = haul.GetComponent<TMP_Text>().font;
+                screenLabel = new GameObject();
+                screenLabel.SetActive(false);
+                screenLabel.name = "ListDeadPlayers";
+                screenLabel.AddComponent<TextMeshProUGUI>();
+
+                screenLabelText = screenLabel.GetComponent<TextMeshProUGUI>();
+                screenLabelText.font = font;
+                // screenLabelText.color = Color.red;
+                screenLabelText.fontSize = 24f;
+                screenLabelText.enableWordWrapping = true;
+                screenLabelText.overflowMode = TextOverflowModes.Overflow;
+                screenLabelText.alignment = TextAlignmentOptions.BottomRight;
+                screenLabelText.horizontalAlignment = HorizontalAlignmentOptions.Right;
+                screenLabelText.verticalAlignment = VerticalAlignmentOptions.Bottom;
+                screenLabelText.SetText("");
+                screenLabel.transform.SetParent(hud.transform, false);
+
+                RectTransform component = screenLabel.GetComponent<RectTransform>();
+
+                component.anchorMax = new Vector2(1f, 0f);
+                component.anchorMin = new Vector2(1f, 0f);
+                component.anchoredPosition = new Vector2(-50f, 50f);
+                component.pivot = new Vector2(1f, 0f);
+                component.sizeDelta = new Vector2(300f, 400f);
+            }
+        }
+    }
+    public void SetupImage()
+    {
+        lock (_updateLock)
+        {
+            if (screenImage == null)
+            {
+                Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID}: Creating image...");
+
+                GameObject hud = GameObject.Find("Game Hud");
+                GameObject haul = GameObject.Find("Tax Haul");
+
+                if (hud == null || haul == null)
+                {
+                    Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID}: Error getting HUD or Haul - not setting up image :(");
+                    return;
+                }
+
+
+                screenImage = new GameObject("MoonImage");
+                screenImage.SetActive(false);
+
+                // Добавляем компонент RawImage
+                screenImageTexture = screenImage.AddComponent<RawImage>();
+                // screenImageTexture.texture = texture;
+                screenImageTexture.color = Color.white;
+
+                // Добавляем в иерархию
+                screenImage.transform.SetParent(hud.transform, false);
+
+                // Настройка позиции и размеров
+                RectTransform rectTransform = screenImage.GetComponent<RectTransform>();
+
+// Привязка к верхнему правому углу
+                rectTransform.anchorMin = new Vector2(1f, 1f);
+                rectTransform.anchorMax = new Vector2(1f, 1f);
+                rectTransform.pivot = new Vector2(1f, 1f);
+
+// Задаём отступы: 60px от правого и 20px от верхнего
+                rectTransform.anchoredPosition = new Vector2(-70f, -10f);
+
+// Размер сохраняется
+                rectTransform.sizeDelta = new Vector2(30f, 30f);
+
+                // screenImage.SetActive(true); // Активируем после настройки
+            }
+        }
+    }
+
 }   
--- patches/MoonPhaseClass.cs
+++ patches/MoonPhaseClass.cs
@@ -2,7 +2,11 @@
 using System.Reflection;
 using BepInEx.Logging;
 using HarmonyLib;
+using MenuLib;
+using MoonPhaseUtils.UI;
 using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace MoonPhaseUtils.patches;
 
@@ -10,50 +14,78 @@ namespace MoonPhaseUtils.patches;
 public class MoonPhaseClass
 {
     internal static ManualLogSource Logger = Plugin.Logger;
+
     [HarmonyPostfix]
-        public static void GenerateDonePatched(LevelGenerator __instance, PhotonMessageInfo _info)
+    public static void GenerateDonePatched(LevelGenerator __instance, PhotonMessageInfo _info)
+    {
+        Logger.LogInfo($"Instance state - {__instance.State}");
+        if (RunManager.instance != null)
         {
-            int __moonLevel = -1;
-            Logger.LogInfo($"Instance state - {__instance.State}");
-            if (__instance.State == LevelGenerator.LevelState.Done)
+            bool isReady = RunManager.instance.levelCurrent != RunManager.instance.levelLobby
+                           && RunManager.instance.levelCurrent != RunManager.instance.levelMainMenu
+                           && RunManager.instance.levelCurrent != RunManager.instance.levelLobbyMenu
+                           && RunManager.instance.levelCurrent != RunManager.instance.levelRecording
+                           && RunManager.instance.levelCurrent != RunManager.instance.levelShop
+                           && RunManager.instance.levelCurrent != RunManager.instance.levelTutorial;
+            if (__instance.State == LevelGenerator.LevelState.Done && isReady)
             {
-                if (RunManager.instance != null)
+                var moonLevelField = AccessTools.Field(typeof(RunManager), "moonLevel");
+                if (moonLevelField != null)
                 {
-                    var moonLevelField = AccessTools.Field(typeof(RunManager),"moonLevel");
-                    if (moonLevelField != null) 
+                    var __moonLevel = (int)moonLevelField.GetValue(RunManager.instance);
+                    List<Moon> moons = (List<Moon>)AccessTools.Field(typeof(RunManager), "moons")
+                        .GetValue(RunManager.instance);
+                    Logger.LogInfo("Available Moons: ");
+                    foreach (Moon moon in moons)
                     {
-                    
-                        __moonLevel = (int)moonLevelField.GetValue(RunManager.instance);
-                        if (__moonLevel > 4)
-                        {
-                            Logger.LogInfo("MoonLevel more that 4 (currently is "+ __moonLevel + "). Setting to 1");
-                            moonLevelField.SetValue(RunManager.instance, 1);
-                        }
-                        List<Moon> moons = (List<Moon>)AccessTools.Field(typeof(RunManager),"moons").GetValue(RunManager.instance);
-                        Logger.LogInfo("Available Moons: ");
-                        foreach (Moon moon in moons)
-                        {
-                            Logger.LogInfo("- " + moon.moonName + " (" + string.Join("||", moon.moonAttributes) + ")" + "(" + moon.moonIcon.ToString() + ")");
-                        }
+                        Logger.LogInfo("- " + moon.moonName + " (" + string.Join("||", moon.moonAttributes) + ")" +
+                                       "(" + moon.moonIcon.ToString() + ")");
                     }
-                    else
+                    //    TopUIBanner banner  here
+                    // TopUIBanner.Show("TEST", RunManager.instance.MoonGetIcon(__moonLevel));
+
+                    if (Plugin.Instance != null)
                     {
-                    
-                        // тут можешь делать что угодно с этим значением
-                        Logger.LogInfo($"MoonLevelField is null. Current level - {__instance.Level}");
+                        // GameObject[] activeGameObjects = Object.FindObjectsOfType<GameObject>();
+                        // foreach (GameObject go in activeGameObjects)
+                        // {
+                        //     Logger.LogInfo($"Found game object {go.name}");
+                        // }
+                        Plugin.Instance.SetupLabel();
+                        Plugin.Instance.SetupImage();
+                        // string text = $"Dead players: test/{GameDirector.instance.PlayerList.Count}\n" 
+                        //               + string.Join("\n", "test2");
+
+                        string text = string.Concat(new string[]
+                        {
+                            "<color=#",
+                            ColorUtility.ToHtmlStringRGB(Color.white),
+                            "><b>",
+                            RunManager.instance.MoonGetName(__moonLevel).Trim(),
+                            "</b>" + "\n" +
+                            string.Join("\n", RunManager.instance.MoonGetAttributes(__moonLevel)),
+                            "</color>"
+                        });
+                        Plugin.Instance.screenLabelText.SetText(text);
+                        // Plugin.Instance.screenLabelText.color = Color.white;
+                        Plugin.Instance.screenLabelText.fontSizeMax = 10f;
+                        Plugin.Instance.screenLabelText.fontSize = 12f;
+                        Plugin.Instance.screenLabel.SetActive(__moonLevel != 0);
+                        Plugin.Instance.screenImageTexture.texture = RunManager.instance.MoonGetIcon(__moonLevel);
+                        Plugin.Instance.screenImage.SetActive(true);
                     }
                 }
                 else
                 {
-                    Logger.LogWarning("RunManager.instance is null.");
-                    return;
+                    // тут можешь делать что угодно с этим значением
+                    Logger.LogInfo($"MoonLevelField is null. Current level - {__instance.Level}");
                 }
             }
-            else
-            {
-                
-                // Logger.LogInfo($"Instance NOT generated - {__instance.Level}");
-            }
-            
         }
+        else
+        {
+            Logger.LogWarning("RunManager.instance is null.");
+            return;
+        }
+    }
 }
