e5d7612e43b89aeeb345bcb3e8a449003d398efe
Heorhii <olenchenko.h.m@nmu.one>
2025-11-01 18:08:45 +0200

    fix: Fixed SaveFileSelected menu

--- patches/MenuPageSavesPatchClass.cs
+++ patches/MenuPageSavesPatchClass.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx.Logging;
+﻿using System.Collections.Generic;
+using BepInEx.Logging;
 using HarmonyLib;
 using TMPro;
 using UnityEngine;
@@ -11,7 +12,7 @@ public class MenuPageSavesPatchClass
     internal static ManualLogSource Logger = Plugin.Logger;
 
     [HarmonyPrefix]
-    private static bool SaveFileSelectedPatched(MenuPageSaves __instance, string saveFileName)
+    private static bool SaveFileSelectedPatched(MenuPageSaves __instance, string saveFolderName, List<string> saveFileBackups)
     {
         MenuElementAnimations component = __instance.saveFileInfo.GetComponent<MenuElementAnimations>();
         component.UIAniNudgeX(10f, 0.2f, 1f);
@@ -21,71 +22,75 @@ public class MenuPageSavesPatchClass
         Image saveFileInfoPanel = (Image)GetField("saveFileInfoPanel", __instance);
         saveFileInfoPanel.color = new Color(0f, 0.1f, 0.25f, 1f);
         SetField("saveFileInfoPanel", saveFileInfoPanel, __instance);
-        string text = StatsManager.instance.SaveFileGetTeamName(saveFileName);
-        string text2 = StatsManager.instance.SaveFileGetDateAndTime(saveFileName);
-        __instance.saveFileHeader.text = text;
-        __instance.saveFileHeader.color = new Color(1f, 0.54f, 0f);
-        __instance.saveFileHeaderDate.text = text2;
-        SetField("currentSaveFileName", saveFileName, __instance);
-        string str = "      ";
-        float time = StatsManager.instance.SaveFileGetTimePlayed(saveFileName);
-        int num = int.Parse(StatsManager.instance.SaveFileGetRunLevel(saveFileName)) + 1;
-        string text3 = ColorUtility.ToHtmlStringRGB(SemiFunc.ColorDifficultyGet(1f, 10f, (float)num));
-        string text4 = StatsManager.instance.SaveFileGetRunCurrency(saveFileName);
-        __instance.saveFileInfoRow1.text = string.Concat(new string[]
+        SetField("currentSaveFileName", saveFolderName, __instance);
+        SetField("currentSaveFileBackups", saveFileBackups, __instance);
+        
+        string str1 = saveFolderName;
+        int result1;
+        int result2;
+        int result3;
+        if (!int.TryParse(StatsManager.instance.SaveFileGetRunLevel(saveFolderName), out result1) || 
+            !int.TryParse(StatsManager.instance.SaveFileGetRunCurrency(saveFolderName), out result2) || 
+            !int.TryParse(StatsManager.instance.SaveFileGetTotalHaul(saveFolderName), out result3))
         {
-            "<sprite name=truck>  <color=#",
-            text3,
-            "><b>",
-            num.ToString(),
-            "</b></color>"
-        });
-        TextMeshProUGUI textMeshProUGUI = __instance.saveFileInfoRow1;
-        textMeshProUGUI.text += str;
-        TextMeshProUGUI textMeshProUGUI2 = __instance.saveFileInfoRow1;
-        textMeshProUGUI2.text = textMeshProUGUI2.text + "<sprite name=clock>  " +
-                                SemiFunc.TimeToString(time, true, new Color(0.1f, 0.4f, 0.8f),
-                                    new Color(0.05f, 0.3f, 0.6f));
-        TextMeshProUGUI textMeshProUGUI3 = __instance.saveFileInfoRow1;
-        textMeshProUGUI3.text += str;
-        string text5 = ColorUtility.ToHtmlStringRGB(new Color(0.2f, 0.5f, 0.3f));
-        TextMeshProUGUI textMeshProUGUI4 = __instance.saveFileInfoRow1;
-        textMeshProUGUI4.text = string.Concat(new string[]
-        {
-            textMeshProUGUI4.text,
-            "<sprite name=$$>  <b>",
-            text4,
-            "</b><color=#",
-            text5,
-            ">k</color>"
-        });
-        string text6 = SemiFunc.DollarGetString(int.Parse(StatsManager.instance.SaveFileGetTotalHaul(saveFileName)));
-        __instance.saveFileInfoRow2.text = string.Concat(new string[]
-        {
-            "<color=#",
-            text5,
-            "><sprite name=$$$> TOTAL HAUL:      <b></b>$ </color><b>",
-            text6,
-            "</b><color=#",
-            text5,
-            ">k</color>"
-        });
-        int num2 = RunManager.instance.CalculateMoonLevel(num - 1);
-        // Logger.LogInfo("MoonLevel - " + num2 + " (num-1=" + num + ")");
-        num2 = Mathf.Clamp(num2, 0, RunManager.instance.moons.Count);
-        if (num2 > 0)
-        {
-            __instance.saveFileInfoMoonRect.gameObject.SetActive(true);
-            __instance.saveFileInfoMoonImage.texture = RunManager.instance.MoonGetIcon(num2);
+            SetField("currentSaveFileValid", false, __instance);
+            if (saveFileBackups.Count > 0)
+                str1 = saveFileBackups[0];
+            if (saveFolderName == str1 || 
+                !int.TryParse(StatsManager.instance.SaveFileGetRunLevel(saveFolderName, str1), out result1) || 
+                !int.TryParse(StatsManager.instance.SaveFileGetRunCurrency(saveFolderName, str1), out result2) || 
+                !int.TryParse(StatsManager.instance.SaveFileGetTotalHaul(saveFolderName, str1), out result3))
+            {
+                ((GameObject)GetField("saveFileInfoLoadButton", __instance)).SetActive(false);
+                ((GameObject)GetField("saveFileInfoRestoreButton", __instance)).SetActive(false);
 
-            // Logger.LogInfo("MoonGetIcon - " + num2 + " (texture=" + __instance.saveFileInfoMoonImage.texture + ")");
+                __instance.saveFileHeader.text = "CORRUPTED SAVE FILE";
+                __instance.saveFileHeader.color = new Color(1f, 0.0f, 0f);
+                __instance.saveFileHeaderDate.text = ":(";
+                __instance.saveFileInfoRow1.text = "Sorry!";
+                __instance.saveFileInfoRow2.text = "";
+                __instance.saveFileInfoMoonRect.gameObject.SetActive(false);
+                __instance.saveFileInfoRow3.text = "Press \"Delete Save\" to delete \nthis save file.";
+                return false;
+            }
+            ((GameObject)GetField("saveFileInfoLoadButton", __instance)).SetActive(false);
+            ((GameObject)GetField("saveFileInfoRestoreButton", __instance)).SetActive(true);
         }
         else
         {
-            __instance.saveFileInfoMoonRect.gameObject.SetActive(false);
+            SetField("currentSaveFileValid", true, __instance);
+            ((GameObject)GetField("saveFileInfoLoadButton", __instance)).SetActive(true);
+            ((GameObject)GetField("saveFileInfoRestoreButton", __instance)).SetActive(false);
         }
-
-        ExecuteMethod("InfoPlayerNames", __instance, new object[] { __instance.saveFileInfoRow3, saveFileName });
+        __instance.saveFileHeader.text = StatsManager.instance.SaveFileGetTeamName(saveFolderName, str1);
+        __instance.saveFileHeader.color = new Color(1f, 0.54f, 0f);
+        __instance.saveFileHeaderDate.text = StatsManager.instance.SaveFileGetDateAndTime(saveFolderName);
+        string str2 = "      ";
+        __instance.saveFileInfoRow1.text = 
+            $"<sprite name=truck>  " +
+            $"<color=#{ColorUtility.ToHtmlStringRGB(SemiFunc.ColorDifficultyGet(1f, 10f, (float) result1 + 1f))}>" +
+            $"<b>{(result1 + 1).ToString()}</b></color>";
+        __instance.saveFileInfoRow1.text += str2; 
+        float timePlayed = StatsManager.instance.SaveFileGetTimePlayed(saveFolderName, str1);
+        TextMeshProUGUI saveFileInfoRow1_1 = __instance.saveFileInfoRow1;
+        saveFileInfoRow1_1.text += 
+            $"{saveFileInfoRow1_1.text}<sprite name=clock>  {SemiFunc.TimeToString(timePlayed, true, new Color(0.1f, 0.4f, 0.8f), new Color(0.05f, 0.3f, 0.6f))}";
+        __instance.saveFileInfoRow1.text += str2;
+        string htmlStringRgb = ColorUtility.ToHtmlStringRGB(new Color(0.2f, 0.5f, 0.3f));
+        
+        TextMeshProUGUI saveFileInfoRow1_2 = __instance.saveFileInfoRow1;
+        saveFileInfoRow1_2.text = $"{saveFileInfoRow1_2.text}<sprite name=$$>  <b>{result2.ToString()}</b><color=#{htmlStringRgb}>k</color>";
+        string str3 = SemiFunc.DollarGetString(result3);
+        __instance.saveFileInfoRow2.text = $"<color=#{htmlStringRgb}><sprite name=$$$> TOTAL HAUL:      <b></b>$ </color><b>{str3}</b><color=#{htmlStringRgb}>k</color>";
+        int _moonIndex = Mathf.Clamp(RunManager.instance.CalculateMoonLevel(result1), 0, RunManager.instance.moons.Count);
+        if (_moonIndex > 0)
+        {
+            __instance.saveFileInfoMoonRect.gameObject.SetActive(true);
+            __instance.saveFileInfoMoonImage.texture = RunManager.instance.MoonGetIcon(_moonIndex);
+        }
+        else
+            __instance.saveFileInfoMoonRect.gameObject.SetActive(false);
+        ExecuteMethod("InfoPlayerNames", __instance, new object[] { __instance.saveFileInfoRow3, saveFolderName, str1 });
         return false;
     }
 
